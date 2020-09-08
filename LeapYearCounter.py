03829ac73d25ee08bf1563a2ee57173e705153c2
MyWiki <giga.anchabadze@gtu.ge>
2020-09-08 13:15:00 +0400

    Py example

---
+++ LeapYearCounter.py
@@ -0,0 +1,15 @@
+year = int(input("The year:"))
+#
+# Put your code here
+
+if (year % 4) == 0:  
+   if (year % 100) == 0:  
+       if (year % 400) == 0:  
+           print("{0} is a leap year".format(year))  
+       else:  
+           print("{0} is not a leap year".format(year))  
+   else:  
+       print("{0} is a leap year".format(year))  
+else:  
+   print("{0} is not a leap year".format(year))  
+
