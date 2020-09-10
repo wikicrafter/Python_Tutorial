8f75c725ca6140fed1113db796f7244872e7c872
MyWiki <giga.anchabadze@gtu.ge>
2020-09-10 23:06:12 +0400

    example

---
+++ OddandEvenNumberCounter.py
@@ -0,0 +1,26 @@
+''' A program that reads a sequence of numbers
+ and counts how many numbers are even and how many are odd.
+ The program terminates when zero is entered. '''
+
+
+odd_numbers = 0
+even_numbers = 0
+
+# read the first number
+number = int(input("Enter a number or type 0 to stop: "))
+
+# 0 terminates execution
+while number != 0:
+    # check if the number is odd
+    if number % 2 == 1:
+        # increase the odd_numbers counter
+        odd_numbers += 1
+    else:
+        # increase the even_numbers counter
+        even_numbers += 1
+    # read the next number
+    number = int(input("Enter a number or type 0 to stop: "))
+
+# print results
+print("Odd numbers count:", odd_numbers)
+print("Even numbers count:", even_numbers)
