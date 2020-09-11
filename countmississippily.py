4c6cff9b6aae6b279eefe117d8452c06d7ae7e34
MyWiki <giga.anchabadze@gtu.ge>
2020-09-11 18:21:01 +0400

    update

---
+++ countmississippily.py
@@ -0,0 +1,11 @@
+import time
+
+# Write a for loop that counts to five.
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    # Body of the loop - use: time.sleep(1)
+
+# Write a print function with the final message.
+
+for i in range(1,5):
+    time.sleep(1)
+print("Ready or not, here I come!")
