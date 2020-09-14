bf91f1419d76c6ef8c764c3346c463417ca55a5c
MyWiki <giga.anchabadze@gtu.ge>
2020-09-14 13:55:52 +0400

    up

---
+++ interactivebubblesorting.py
@@ -0,0 +1,17 @@
+myList = []
+swapped = True
+num = int(input("How many elements do you want to sort: "))
+
+for i in range(num):
+    val = int(input("Enter a list element: "))
+    myList.append(val)
+
+while swapped:
+    swapped = False
+    for i in range(len(myList) - 1):
+        if myList[i] > myList[i + 1]:
+            swapped = True
+            myList[i], myList[i + 1] = myList[i + 1], myList[i]
+
+print("\nSorted:")
+print(myList)
---
+++ thebubblesortalgorithm.py
@@ -0,0 +1,11 @@
+myList = [5, 23, 18, 18, 19, 12, 10, 6, 2, 4] # list to sort
+swapped = True # it's a little fake - we need it to enter the while loop
+
+while swapped:
+    swapped = False # no swaps so far
+    for i in range(len(myList) - 1):
+        if myList[i] > myList[i + 1]:
+            swapped = True # swap occured!
+            myList[i], myList[i + 1] = myList[i + 1], myList[i]
+
+print(myList)
