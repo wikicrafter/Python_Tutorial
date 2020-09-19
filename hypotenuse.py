3517fad2eca6b8b972b27375c071b3013c383239
MyWiki <giga.anchabadze@gtu.ge>
2020-09-19 13:13:44 +0400

    up

---
+++ hypotenuse.py
@@ -0,0 +1,13 @@
+def isItATriangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+
+def isItRightTriangle(a, b, c):
+    if not isItATriangle(a, b, c):
+        return False
+    if c > a and c > b:
+        return c ** 2 == a ** 2 + b ** 2
+    if a > b and a > c:
+        return a ** 2 == b ** 2 + c ** 2
+
+print(isItRightTriangle(5, 3, 4))
+print(isItRightTriangle(1, 3, 4))
