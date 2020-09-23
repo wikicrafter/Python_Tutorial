dfcd253de14a360607425ceb8c3ec3436e281c1a
MyWiki <giga.anchabadze@gtu.ge>
2020-09-24 02:14:55 +0400

    up

---
+++ mycalculation.py
@@ -0,0 +1,18 @@
+
+def mfc(a,b):
+  print(a+b, " " "addition")
+  print(a-b, " " "subtraction")
+  print(a*b, " " "multiplication")
+  print(a/b, " " "division")
+  print(a**b, " " "exponents")
+  print(a%b, " " "modulus")
+  return
+a = int(input(" " "number = "))
+b = int(input(" " "number = "))
+
+mfc(a, b)
+
+
+  
+  
+  
