0132c7d654abf72950a564b6fefcfd9f9e9203fc
MyWiki <giga.anchabadze@gtu.ge>
2020-09-25 00:49:20 +0400

    up

---
+++ largestnumber.py
@@ -0,0 +1,11 @@
+a=int(input("enter N="))
+b=int(input("enter N="))
+c=int(input("enter N="))
+if a>b and a>c:
+  print("First is a largest number");
+if b>a and b>c:
+  print("second is a largest number");
+if c>a and c>b:
+  print("third is a largest number");
+
+ 
---
+++ mynameandagefunction.py
@@ -0,0 +1,6 @@
+def printme(name,age):    
+    print("My name is",name,"and age is",age)    
+
+name = "Giga"
+age = input("age")
+print(name, age)  
