044b73fe732a8b5bd378bbe3b76278d07230520a
MyWiki <giga.anchabadze@gtu.ge>
2020-09-27 00:42:06 +0400

    up

---
+++ mostcommondata.py
@@ -0,0 +1,8 @@
+import statistics     
+# declaring a simple data-set consisting of real valued positive integers.   
+dataset =[2, 4, 7, 7, 2, 2, 3, 6, 6, 8]     
+# Printing out the mode of given data-set   
+print("Calculated Mode % s" % (statistics.mode(dataset))) 
+
+# output
+# calculated mode 2
---
+++ randomnumbergenerator.py
@@ -0,0 +1,9 @@
+import random
+
+# We are using the choice() function to generate a random number from  
+# the given list of numbers.  
+
+print ("The random number from list is : ",end="")  
+
+print(random.choice([23, 45, 63, 656, 342, 34, 3, 434, 2, 34, 997, 1997, 1999, 2020, 21]))
+
